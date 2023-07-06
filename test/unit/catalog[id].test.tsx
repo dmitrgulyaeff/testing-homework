dfed97e2a34606c19ef704f3bde0d2d176496abf
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-07-06 22:33:16 +0700

    feat: Add test for BugID 9

--- test/unit/catalog[id].test.tsx
+++ test/unit/catalog[id].test.tsx
@@ -10,11 +10,22 @@ describe('Каталог[id]', () => {
   if (products) {
     for (const product of products) {
       testProductDescription(details[product.id], initialState);
+      testProductButton(details[product.id], initialState);
     }
+  }
+  
+  function testProductButton(product: Product, state: ApplicationState) {
+    it('Ожидаемый размер кнопки: btn-lg', () => {
+      const { render } = renderPage(`/catalog/${product.id}`, state);
+      const { container } = render;
+      const btnAddToCart = container.querySelector('.ProductDetails-AddToCart');
+      const classList = btnAddToCart?.getAttribute('class')?.split(' ');
+      expect(classList).toContain('btn-lg');
+    });
   }
 
   function testProductDescription(product: Product, state: ApplicationState) {
-    it(`На странице с подробной информацией продукта ${product.id} отображаются: название товара, его описание, цена, цвет, материал и кнопка "добавить в корзину"`, () => {
+    it(`На странице с подробной информацией продукта ${product.id} отображаются: название товара, его описание, цена, цвет, материал и кнопка "добавить в корзину".`, () => {
       const { render } = renderPage(`/catalog/${product.id}`, state);
       const { container } = render;
 
@@ -46,5 +57,4 @@ describe('Каталог[id]', () => {
       testField('AddToCart', 'Add to Cart');
     });
   }
-
-})
+});
