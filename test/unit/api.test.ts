75dfa90aa259b0d22bdbe880db946bb9c69cbbba
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-07-07 02:14:42 +0700

    feat: Add test for BugID 3

--- test/unit/api.test.ts
+++ test/unit/api.test.ts
@@ -22,15 +22,37 @@ describe('Проверка эндпоинта', () => {
 
     const products = response.body;
     expect(Array.isArray(products)).toBe(true);
-
     expect(products).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
           id: expect.any(Number),
           name: expect.any(String),
-          price: expect.any(Number)
-        })
+          price: expect.any(Number),
+        }),
       ])
     );
   });
+
+   const checkProductInfo = (id: number) => {
+    it(`/hw/store/api/products/${id} должен возвращать объект с {id: ${id}, name, description, price, color, material}`, async () => {
+      const response = await request(domainApi)
+        .get(`/hw/store/api/products/${id}`)
+        .expect(200);
+      const productsInfo = response.body;
+      expect(productsInfo).toEqual(
+        expect.objectContaining({
+          id: id,
+          name: expect.any(String),
+          description: expect.any(String),
+          price: expect.any(Number),
+          color: expect.any(String),
+          material: expect.any(String),
+        })
+      );
+    });
+  }
+
+  for (let i = 0; i < 5; i++) {
+    checkProductInfo(i);
+  }
 });
